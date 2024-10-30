9d09d8a0c281406f2713562705f6ec9a28072b7f
Guðmundur Garðar Árnason <gardar39@gmail.com>
2024-10-30 21:00:32 +0000

    cleanup routes

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,22 +1,12 @@
-import Footer from "../footer/page";
-import Header from "../header/page";
-import Layout from "./layout";
-
 export default function Gallery() {
   return (
-    <>
-      <Header />
-      <Layout>
-        <div className="flex flex-col items-center">
-          <div className="mt-14 text-5xl text-center font-semibold">
-            <h1>Shrek & Friends Gallery</h1>
-          </div>
-          <div className="mt-36">
-            <img src="/images/gallery.png" alt="Gallery" width={450} height={450} />
-          </div>
-        </div>
-      </Layout>
-      <Footer />
-    </>
+    <div className="flex flex-col items-center">
+      <div className="mt-14 text-5xl text-center font-semibold">
+        <h1>Shrek & Friends Gallery</h1>
+      </div>
+      <div className="mt-36">
+        <img src="/images/gallery.png" alt="Gallery" width={450} height={450} />
+      </div>
+    </div>
   );
 }
